dd6a3d2abb11b40b60be33248641a276c164c600
Ahmed Samy <serenitydiver@hotmail.com>
2019-09-06 17:56:07 +0300

    Working with Annotation and mapping resource

--- src/main/java/com.javamsdt/Main.java
+++
@@ -1,7 +0,0 @@
-package com.javamsdt;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hibernate Xml");
-    }
-}
---
+++ src/main/java/com/javamsdt/hibernatexml/model/Model.java
@@ -0,0 +1,4 @@
+package com.javamsdt.hibernatexml.model;
+
+public class Model {
+}
