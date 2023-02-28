ff99edffea4e75c0e409ce746966ea3b402d0a02
Ahmed Samy <serenitydiver@hotmail.com>
2023-02-28 15:59:16 +0100

    Working on First Task Creating DB configuration

---
+++ 01-Spring-Foundation/01-TaskOne-DbConfig/dbconfig/src/test/java/com/spring/dbconfig/DbConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.dbconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
